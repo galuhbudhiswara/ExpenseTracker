f1c34908574e4ccd67b7c52d64d9ded082d85f55
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-06-02 19:49:21 +0700

    adding constructor fonctions and filtering list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,13 +40,18 @@ class ExpenseBucket {
     required this.expenses,
   });
 
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
   final Category category;
   final List<Expense> expenses;
 
   double get totalExpenses {
     double sum = 0;
 
-    for(final expense in expenses) {
+    for (final expense in expenses) {
       sum += expense.amount; // sum + sum = expense.amount
     }
 
