868cdc67b99d31f25f40c1d4e94c49588e6634c9
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-06-02 22:21:13 +0700

    layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 216, 102),
@@ -11,9 +11,9 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) {
     runApp(
       MaterialApp(
         darkTheme: ThemeData.dark().copyWith(
@@ -64,5 +64,5 @@ void main() {
         home: const Expenses(),
       ),
     );
-  });
+  // });
 }
